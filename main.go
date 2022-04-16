44f808d76a6581ff2d3435469f2ef8b23deaf3fd
Suntipap <88478908+Suntipap1312@users.noreply.github.com>
2022-04-16 22:29:57 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"main/model"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func GetDB() *gorm.DB {
+	return db
+}
+
+func ConnectDB() {
+	database, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	database.AutoMigrate(&model.User{}, &model.Product{}, &model.Transaction{})
+	db = database
+}
